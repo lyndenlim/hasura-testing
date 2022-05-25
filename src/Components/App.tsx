a89bf201a5ac92054d339ffb82e507d30b00bbae
lynden <lyndenlim15@gmail.com>
2022-05-25 15:22:14 -0400

    Setup apollo client

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
+import { gql, useQuery } from "@apollo/client"
+
+const GET_USERS = gql`
+  {
+    users {
+      email
+    }
+  }
+`
 
 function App() {
+  const { loading, error, data } = useQuery(GET_USERS)
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <div className="App">
-      Hasura
+      {data.users.email}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "https://moved-dogfish-81.hasura.app/v1/graphql",
+  cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
