b9e8a8e873e1e2276226725e226613b5ed8a1ce9
lynden <lyndenlim15@gmail.com>
2022-05-26 01:04:37 -0400

    Edit function

--- src/Components/DisplayUser.tsx
+++ src/Components/DisplayUser.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery, useMutation } from "@apollo/client";
+import { useState } from "react";
 
 export const GET_USERS = gql`
   {
@@ -18,19 +19,39 @@ const DELETE_USER = gql`
   }
 `;
 
+const EDIT_USER = gql`
+  mutation EditUser(
+    $_set: users_set_input
+    $pk_columns: users_pk_columns_input!
+  ) {
+    update_users_by_pk(_set: $_set, pk_columns: $pk_columns) {
+      id
+      email
+      username
+    }
+  }
+`;
+
 interface User {
   id: number;
   email: string;
   username: string;
 }
 const DisplayUser: React.FC = () => {
+  const [newEmail, setNewEmail] = useState<String>("");
+  const [newUsername, setNewUsername] = useState<String>("");
+
   const {
     loading: usersQueryLoading,
     error: usersQueryError,
     data: users,
   } = useQuery(GET_USERS);
 
-  const [deleteUser, { loading, error, data }] = useMutation(DELETE_USER, {
+  const [deleteUser] = useMutation(DELETE_USER, {
+    refetchQueries: [GET_USERS],
+  });
+
+  const [editUser] = useMutation(EDIT_USER, {
     refetchQueries: [GET_USERS],
   });
 
@@ -44,6 +65,29 @@ const DisplayUser: React.FC = () => {
           {user.username}
           <br />
           {user.email}
+          <br />
+          <input
+            placeholder="New Username"
+            onChange={(e) => setNewUsername(e.target.value)}
+          />
+          <input
+            placeholder="New Email"
+            onChange={(e) => setNewEmail(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              setNewEmail("");
+              setNewUsername("");
+              editUser({
+                variables: {
+                  _set: { email: newEmail, username: newUsername },
+                  pk_columns: { id: user.id },
+                },
+              });
+            }}
+          >
+            Edit
+          </button>
           <br />
           <button onClick={() => deleteUser({ variables: { id: user.id } })}>
             Delete
